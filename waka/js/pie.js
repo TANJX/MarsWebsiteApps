29cdec380c4ec112adac2688008e9810b73437c0
Jianxuan Tan <jianxuat@usc.edu>
2019-03-28 20:00:35 -0700

    waka: show project total time on legend

--- waka/js/pie.js
+++ waka/js/pie.js
@@ -4,32 +4,32 @@
  * @requires Chart
  */
 function pie_chart(user, field, element) {
-    $.ajax({
-        url: "http://api.marstanjx.com:3000/waka/chart/" + field + "/" + user + "", success: function (result) {
-            const ctx = document.getElementById(element).getContext('2d');
+  $.ajax({
+    url: "http://api.marstanjx.com:3000/waka/chart/" + field + "/" + user + "", success: function (result) {
+      const ctx = document.getElementById(element).getContext('2d');
 
-            new Chart(ctx, {
-                type: 'doughnut',
-                data: result,
-                options: {
-                    tooltips: {
-                        callbacks: {
-                            label: function (tooltipItem, data) {
-                                let label = data.labels[tooltipItem.index] || '';
+      new Chart(ctx, {
+        type: 'doughnut',
+        data: result,
+        options: {
+          tooltips: {
+            callbacks: {
+              label: function (tooltipItem, data) {
+                let label = data.labels[tooltipItem.index] || '';
 
-                                if (label) {
-                                    label += ': ';
-                                }
-                                label += getTimeString(data.datasets[0].data[tooltipItem.index]);
-                                return label;
-                            }
-                        }
-                    },
-                    legend: {
-                        position: 'right',
-                    }
+                if (label) {
+                  label += ': ';
                 }
-            });
+                label += getTimeString(data.datasets[0].data[tooltipItem.index]);
+                return label;
+              }
+            }
+          },
+          legend: {
+            position: 'right',
+          }
         }
-    });
+      });
+    }
+  });
 }
--- waka/js/projects.js
+++ waka/js/projects.js
@@ -4,47 +4,65 @@
  * @requires Chart
  */
 function project_chart(user, element) {
-    $.ajax({
-        url: "http://api.marstanjx.com:3000/waka/chart/project/" + user + "", success: function (result) {
-            const ctx = document.getElementById(element).getContext('2d');
-            new Chart(ctx, {
-                type: 'bar',
-                data: result,
-                options: {
-                    tooltips: {
-                        mode: 'x',
-                        filter: function (data) {
-                            return data['yLabel'] !== 0;
-                        },
-                        callbacks: {
-                            label: function (tooltipItem, data) {
-                                let label = data.datasets[tooltipItem.datasetIndex].label || '';
+  $.ajax({
+    url: "http://api.marstanjx.com:3000/waka/chart/project/" + user + "", success: function (result) {
+      const ctx = document.getElementById(element).getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: result,
+        options: {
+          tooltips: {
+            mode: 'x',
+            filter: function (data) {
+              return data['yLabel'] !== 0;
+            },
+            callbacks: {
+              label: function (tooltipItem, data) {
+                let label = data.datasets[tooltipItem.datasetIndex].label || '';
 
-                                if (label) {
-                                    label += ': ';
-                                }
-                                label += moment.duration(tooltipItem.yLabel, 'seconds').humanize();
-                                return label;
-                            }
-                        }
-                    },
-                    legend: {
-                        position: 'right',
-                    },
-                    scales: {
-                        yAxes: [{
-                            stacked: true,
-                            ticks: {
-                                beginAtZero: true,
-                                callback: getTimeString
-                            }
-                        }],
-                        xAxes: [{
-                            stacked: true,
-                        }],
-                    }
+                if (label) {
+                  label += ': ';
                 }
-            });
+                label += moment.duration(tooltipItem.yLabel, 'seconds').humanize();
+                return label;
+              }
+            }
+          },
+          legend: {
+            position: 'right',
+            labels: {
+              generateLabels: function (chart) {
+                const data = chart.data;
+                if (data.labels.length && data.datasets.length) {
+                  return data.datasets.map(function (dataset, i) {
+                    const fill = dataset.backgroundColor;
+                    const value = dataset.data.map(obj => obj.y).reduce((result, val) => result + val);
+                    return {
+                      text: dataset.label + " : " + getTimeString(value),
+                      fillStyle: fill,
+                      index: i
+                    };
+                  });
+                } else {
+                  return [];
+                }
+              }
+            }
+          },
+          scales: {
+            yAxes: [{
+              stacked: true,
+              ticks: {
+                beginAtZero: true,
+                callback: getTimeString
+              }
+            }],
+            xAxes: [{
+              stacked: true,
+            }],
+          }
         }
-    });
+      });
+    }
+  });
 }
